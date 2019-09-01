7734ed99c49701050fda21ee4b319990539c7cc4
Sho Yashiro <shirotani@seginus.jp>
2019-09-01 22:59:08 +0900

    機能改善・リファクタリング
    ・出力ファイルをグローバル変数化
    ・実行ファイル名を取得できるようにし、ファイル名称のハードコーディングを削除
    ・Walkメソッドによるトラバース処理の反映

--- listFiles.go
+++ listFiles.go
@@ -3,10 +3,15 @@ package main
 import (
 	"fmt"
 	"os"
-	"io/ioutil"
+	"strings"
 	"path/filepath"
 )
 
+var(
+	output *os.File
+	outputFlNm string
+	exeFlNm string
+)
 /**
  * メイン関数。カレントディレクトリを取得し、出力用のファイルを生成する。
  */
@@ -17,47 +22,52 @@ func main() {
 	//カレントディレクトリ取得
 	cur, _ := os.Getwd()
 
-	//list.txtの生成
-	output, err := os.Create(cur + "/list.txt")
+	var err error
+
+	//exeのファイル名取得
+	thi, err := os.Executable()
+	dirNm := filepath.Dir(thi) + "\\"
+	exeFlNm = strings.Replace(thi, dirNm , "", -1)
+
+	//出力ファイルの生成
+	outputFlNm = "list.txt"
+	output, err = os.Create(cur + "/" + outputFlNm)
 	if err != nil {
-		fmt.Print("カレントディレクトリ取得、またはlist.txtの生成時にエラーが発生しました。")
+		fmt.Print("カレントディレクトリ取得、または出力ファイルの生成時にエラーが発生しました。")
 		return
 	}
 	defer output.Close()
 
-	//ファイル一覧出力関数
-	showFiles(cur, output)
+	//カレントディレクトリをトラバース処理し、ファイル出力関数を呼び出す。
+	filepath.Walk(cur, listFiles)
 
 	//終了メッセージ出力
 	fmt.Print("ファイル一覧の出力を完了しました。")
 }
 
 /**
- * ファイル一覧出力関数。
+ * ファイル出力関数。
  * cur:カレントディレクトリ,output:出力ファイル
  */
-func showFiles(cur string, output *os.File) {
-	//ディレクトリの中身を読み込み開始
-	files, _ := ioutil.ReadDir(cur)
-
-	//中身一覧をforループ
-	for _, file := range files {
-		//取得したものがディレクトリであった場合、再帰させる
-		if file.IsDir(){
-			absolutePath, _ := filepath.Abs(file.Name())
-			showFiles(absolutePath, output)
-			continue
-		}
-		//取得したものがexeファイル、出力ファイルであった場合はスキップする
-		if file.Name() == "listFiles.exe" || file.Name() == "list.txt"{
-			continue
-		}
-
-		//ファイル名、ファイルパスの取得
-		fileName := file.Name()
-		filePath, _ := filepath.Abs(fileName)
-
-		//ファイルへの書き込み
-		output.WriteString(filePath + "\t" + fileName + "\r")
+func listFiles(path string, file os.FileInfo, err error) error {
+	if err != nil{
+		return err
 	}
+
+	//取得したものがexeファイル、出力ファイルであった場合はスキップする
+	if file.Name() == exeFlNm || file.Name() == outputFlNm{
+		return nil
+	}
+
+	//取得したものがディレクトリであった場合、再帰させる
+	if file.IsDir(){
+		return nil
+	}
+
+	//ファイル名、ファイルパスの取得
+	fileName := file.Name()
+	//ファイルへの書き込み
+	output.WriteString(path + "\t" + fileName + "\r")
+
+	return nil
 }
